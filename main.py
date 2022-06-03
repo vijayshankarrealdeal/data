ac199f6dc07fbafbb2e2255c4b90d65e9224f8e8
vijayshankarrealdeal <vijaygaya056@gmail.com>
2022-06-03 22:04:06 +0530

    nnn

--- main.py
+++ main.py
@@ -23,16 +23,9 @@ def predictX(area, day, month, year, state):
     df.states = df.states.astype(str)
     y_pred_1 = pipeline1.predict(df)[0]
     y_pred_2 = pipeline2.predict(df)[0]
-    latitude = round(y_pred_1, 5)
-    longitude = round(y_pred_2, 5)
-    url = f'https://api.bigdatacloud.net/data/reverse-geocode-client?latitude={latitude}&longitude={longitude}&localityLanguage=en'
+    url = f'https://api.bigdatacloud.net/data/reverse-geocode-client?latitude={round(y_pred_1, 5)}&longitude={round(y_pred_2, 5)}&localityLanguage=en'
     data = requests.get(url)
-    x = data.json()
-    continent = x['continent']
-    city = x['city']
-    countryName = x['countryName']
-    localityInfo = x['localityInfo']
-    return {'latitude': y_pred_1.tolist(), 'longitude': y_pred_2.tolist(), 'continent': continent, 'city': city, "countryName": countryName, 'localityInfo': localityInfo}
+    return {'latitude': y_pred_1.tolist(), 'longitude': y_pred_2.tolist(), 'continent': data.json()['continent'], 'city':  data.json()['city'], "countryName":  data.json()['countryName'], 'localityInfo':  data.json()['localityInfo']}
 
 
 
